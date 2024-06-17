020c0798450e502bcde2b7a6001d7bddc1ea17df
Alexandros Papadas <sperako948@gmail.com>
2024-06-17 18:53:03 +0300

    added compress.py which gets a string an uses Shannon Fano to compress it and returns encoded_message,entropy,avg_length

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,23 @@
 import os
 from flask_socketio import SocketIO, send , emit
 
-from flask import Flask, render_template
+from flask import Flask, render_template,flash,request,redirect, url_for
 
 
 def create_app(test_config=None):
     # create and configure the app
+    UPLOAD_FOLDER = 'C:/test'
+    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
     app = Flask(__name__, instance_relative_config=True)
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+
     )
     socket = SocketIO(app)
+
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -26,11 +32,34 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    def allowed_file(filename):
+        return '.' in filename and \
+            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
     @app.route("/")
     def main():
         return render_template("main.html")
 
+    @app.route('/', methods=['GET', 'POST'])
+    def upload_file():
+        if request.method == 'POST':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit an empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                #for now it saves the file
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+                return render_template("main.html")
+
+        return
     @socket.on('message')
     def handle_message(msg):
         socket.send("test")
---
+++ flaskr/compress.py
@@ -0,0 +1,137 @@
+from collections import Counter
+import math
+from collections import OrderedDict
+import pandas as pd
+
+
+
+def symProb(String):
+    sc = dict(Counter(String.upper()));
+    return {k: v / sum(sc.values()) for k, v in sc.items()}
+
+
+
+
+def compressing(string):
+    def best_partition(initial, final):
+        pts = []  # points
+        for i in range(initial + 1, final):
+            diff = abs(sum(sv[initial:i]) - sum(sv[i:final]))
+            pts.append((diff))
+            # print(abs(sum(sv[initial:i])-sum(sv[i:final])),sv[initial:i],sv[i:final],i)
+            # print("\n",[sum(sv[initial:i]),sum(sv[i:final])],"\n")
+        print(pts.index(min(pts)) + initial)
+
+        if pts.index(min(pts)) < initial:
+            return pts.index(min(pts)) + initial;
+        else:
+            return pts.index(min(pts))
+
+    def up(initial, final):
+        for i in range(initial, final): sc[i] = sc[i] + '0'
+
+    def down(initial, final):
+        for i in range(initial, final): sc[i] = sc[i] + '1'
+
+    symd = symProb(string)  # symd-symbols Dictionary
+    symdn = OrderedDict(sorted(symd.items(), key=lambda kv: kv[1], reverse=True))
+    sk = list(symdn.keys())
+    sv = list(symdn.values())
+
+    initial = 0
+    final = len(sv)
+    sc = [''] * (len(sv))
+    current_index = [(initial, final)]
+    new_index = []
+    stage = 1
+    while current_index != []:
+        print("---", "stage:-", stage, "----");
+        print(sc);
+        new_index = [];
+        print(current_index)
+        for index in current_index:
+            if (index[1] - index[0]) == 2: sc[index[0]] = sc[index[0]] + '0'; sc[index[1] - 1] = sc[index[1] - 1] + '1';
+
+            if (index[1] - index[0]) > 2:
+                index_ptr = best_partition(index[0], index[1]) + 1;
+                new_index.append((index[0], index_ptr));
+                up(index[0], index_ptr);
+                new_index.append((index_ptr, index[1]));
+                down(index_ptr, index[1]);
+            current_index = new_index
+        print("\n")
+        stage = stage + 1
+    # for last stage
+    if current_index == []:
+        print("---", "stage:-", stage, "----");
+        print(sc);
+        new_index = [];
+        print(current_index)
+
+    # In[9]:
+
+    dummy = []
+    for i in range(0, len(sv)): dummy.append((sk[i], sc[i]));
+    encoded_dictionary = OrderedDict(dummy)
+
+    # In[10]:
+
+    data = {'Symbol': sk, 'probability': sv, 'codeword': sc, }
+    df = pd.DataFrame(data)
+    df.head(len(sk))
+
+    # # Encoded Message
+
+    # In[11]:
+    encoded_message = ''.join(sc)
+    print("For message:-", string, "\nEncoded message is:-", encoded_message)
+
+    # # Entropy Calculation:-
+
+    # In[12]:
+
+    H = 0
+    for i in range(0, len(sv)): H = H + sv[i] * math.log((1 / sv[i]), 2);
+    entropy = H
+    print("Entropy is:-", H, "bits/message")
+
+    # # Average Codeword Length
+    # In[13]:
+
+    L = 0
+    for i in range(0, len(sc)): L = L + len(sc[i]) * sv[i];
+    avg_length = L
+    print("Average Codeword length is:-", L, "bits/message")
+
+    # # Compression
+
+    # In[14]:
+
+    code_efficiency = (H / L) * 100
+    print("Coding efficiency is :-", (H / L) * 100)
+
+    def best_partition(initial, final):
+        pts = []  # points
+        for i in range(initial + 1, final):
+            diff = abs(sum(sv[initial:i]) - sum(sv[i:final]))
+            pts.append((diff))
+            # print(abs(sum(sv[initial:i])-sum(sv[i:final])),sv[initial:i],sv[i:final],i)
+            # print("\n",[sum(sv[initial:i]),sum(sv[i:final])],"\n")
+        print(pts.index(min(pts)) + initial)
+
+        if pts.index(min(pts)) < initial:
+            return pts.index(min(pts)) + initial;
+        else:
+            return pts.index(min(pts))
+
+    def up(initial, final):
+        for i in range(initial, final): sc[i] = sc[i] + '0'
+
+    def down(initial, final):
+        for i in range(initial, final): sc[i] = sc[i] + '1'
+    return encoded_message,entropy,avg_length
+
+
+#string1 = input("Enter the string:")
+#compress(string1)
+
